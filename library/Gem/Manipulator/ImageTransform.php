0d98e984ba09221ae656a57baa69cb3de467d57c
Johan Nilsson <johan@frank.(none)>
2008-07-23 22:12:41 +0200

    added error handling

--- library/Gem/Manipulator/ImageTransform.php
+++ library/Gem/Manipulator/ImageTransform.php
@@ -10,20 +10,49 @@ class Gem_Manipulator_ImageTransform implements Gem_Manipulator_Interface
 {
     /**
      * Perfoms manipulation
+     * 
+     * TODO: Allow options to decide with image lib to load and how to resize the
+     * image.
      *
-     * @param unknown_type $from
-     * @param unknown_type $to
-     * @param unknown_type $options
+     * @param string $from
+     * @param string $to
+     * @param array  $options
      */
-	public function manipulate($from, $to, $options)
+	public function manipulate($from, $to, array $options = array())
 	{
-		// Needs to be required here, otherwise getting "Cannot access self:: when 
-		// no class scope is active" from PEAR.
+	    $options = new ArrayObject($options);
+	    if (false == $options->offsetExists('width'))
+	    {
+	        throw new Gem_Manipulator_Exception('Missing option for width');
+	    }
+
+		/*
+		 * Needs to be required here, otherwise getting "Cannot access self:: when 
+		 * no class scope is active" from PEAR, probably only in autoload context.
+		 */
 		require_once 'Image/Transform.php';
 
-		$imageTransform = Image_Transform::factory('GD');
-		$imageTransform->load($from);
-		$imageTransform->scaleByX($options['width']);
-		$imageTransform->save($to);
+        /*
+         * Here comes the ugly pear integration...
+         */
+        $imageTransform = Image_Transform::factory('GD');
+        if (PEAR::isError($imageTransform)) {
+            throw new Gem_Manipulator_Exception($imageTransform->getMessage());
+        }
+
+        $response = $imageTransform->load($from);
+        if (PEAR::isError($response)) {
+            throw new Gem_Manipulator_Exception($response->getMessage());
+        }
+
+        $response = $imageTransform->scaleByX($options->offsetGet('width'));
+        if (PEAR::isError($response)) {
+            throw new Gem_Manipulator_Exception($response->getMessage());
+        }        
+
+        $response = $imageTransform->save($to);
+        if (PEAR::isError($response)) {
+            throw new Gem_Manipulator_Exception($response->getMessage());
+        }
 	}
 }
