2db75fa8d548bc856157e172995aab46842d9142
Johan Nilsson <johan@frank.(none)>
2008-07-27 00:17:43 +0200

    ws fix

--- library/Gem/Manipulate/ImageTransform.php
+++ library/Gem/Manipulate/ImageTransform.php
@@ -6,19 +6,19 @@ require_once 'Gem/Manipulate/Interface.php';
  * @category   Gem
  * @package    Gem_Manipulate
  */
-class Gem_Manipulate_ImageTransform implements Gem_Manipulate_Interface 
+class Gem_Manipulate_ImageTransform implements Gem_Manipulate_Interface
 {
     /**
      * Perfoms manipulation
-     * 
+     *
      */
-	public function manipulate($from, $to, $options)
-	{
-		/*
-		 * Needs to be required here, otherwise getting "Cannot access self:: when 
-		 * no class scope is active" from PEAR, probably only in autoload context.
-		 */
-		require_once 'Image/Transform.php';
+    public function manipulate($from, $to, $options)
+    {
+        /*
+         * Needs to be required here, otherwise getting "Cannot access self:: when
+         * no class scope is active" from PEAR, probably only in autoload context.
+         */
+        require_once 'Image/Transform.php';
 
         /*
          * Here comes the ugly pear integration...
@@ -42,5 +42,5 @@ class Gem_Manipulate_ImageTransform implements Gem_Manipulate_Interface
         if (PEAR::isError($response)) {
             throw new Gem_Manipulator_Exception($response->getMessage());
         }
-	}
+    }
 }
