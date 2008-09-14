42278d444b0860ea55b786a8d34b63f102fd278a
Johan Nilsson <johan@frank.(none)>
2008-09-14 17:32:05 +0200

    added posibility to crop uploaded images

--- library/Gem/Manipulate/ImageTransform.php
+++ library/Gem/Manipulate/ImageTransform.php
@@ -14,6 +14,19 @@ class Gem_Manipulate_ImageTransform implements Gem_Manipulate_Interface
      */
     public function manipulate($from, $to, $options)
     {
+        if (!isset($options['size'])) {
+            throw new Gem_Manipulate_Exception('ImageTransform requires the \'size\' option to be set');
+        }
+        $matches = array();
+        preg_match('/(c)?([0-9]+)x([0-9]+)/', $options['size'], $matches);
+
+        if (empty($matches[2])) {
+            throw new Gem_Manipulate_Exception('Invalid size pattern \'' . $options['size']  . '\'');
+        }
+        if (empty($matches[3])) {
+            throw new Gem_Manipulate_Exception('Invalid size pattern \'' . $options['size'] . '\'');
+        }
+
         /*
          * Needs to be required here, otherwise getting "Cannot access self:: when
          * no class scope is active" from PEAR, probably only in autoload context.
@@ -33,12 +46,61 @@ class Gem_Manipulate_ImageTransform implements Gem_Manipulate_Interface
             throw new Gem_Manipulator_Exception($response->getMessage());
         }
 
-        $response = $imageTransform->scaleByX($options['width']);
+        if (empty($matches[1])) {
+            $this->_fit($imageTransform, $matches[2], $matches[3]);        
+        } else {
+            $this->_cropResize($imageTransform, $matches[2], $matches[3]);        
+        }
+
+        $response = $imageTransform->save($to);
+        if (PEAR::isError($response)) {
+            throw new Gem_Manipulator_Exception($response->getMessage());
+        }
+    }
+
+    /**
+     * Fit
+     * 
+     * @param $imageTransform Image_Transform
+     * @param $width int
+     * @param $height int
+     */
+    private function _fit($imageTransform, $width, $height)
+    {
+        $response = $imageTransform->fit($width, $height);
+        if (PEAR::isError($response)) {
+            throw new Gem_Manipulator_Exception($response->getMessage());
+        }
+    }
+
+    /**
+     * Crop and resize
+     * 
+     * @param $imageTransform Image_Transform
+     * @param $width int
+     * @param $height int
+     */
+    private function _cropResize($imageTransform, $width, $height)
+    {
+        $aspectRatio = $imageTransform->getImageWidth() / $imageTransform->getImageHeight();
+        $newAspectRatio = $width / $height;
+
+        if ($newAspectRatio > $aspectRatio) {
+            $newWidth = round(($height * $aspectRatio));
+            $newHeight = $height;
+        } else {
+            $newWidth = $width;
+            $newHeight = round(($width * $aspectRatio));
+        }
+
+        $maxLength = $newWidth >= $newHeight ? $newWidth : $newHeight;
+
+        $response = $imageTransform->scaleMaxLength($maxLength);
         if (PEAR::isError($response)) {
             throw new Gem_Manipulator_Exception($response->getMessage());
         }
 
-        $response = $imageTransform->save($to);
+        $response = $imageTransform->crop($width, $height);
         if (PEAR::isError($response)) {
             throw new Gem_Manipulator_Exception($response->getMessage());
         }
